4f698ac939c4f7e2c34dde18afc591a543ceec1f
Jose Luis <joselo_238@hotmail.com>
2024-04-28 22:21:45 -0500

    commit inicial

---
+++ Catculator-beta.py
@@ -0,0 +1,25 @@
+#Hola!!, esta es la primera cosa que hago en phyton
+print("Calculadora completa pro (≽^•⩊•^≼)୭")
+print("")
+resultado = input("Ingresa el primer numero ≽^•⩊•^≼ :")
+print("")
+resultado2 = input("Ingresa el segundo numero ≽^- ˕ -^≼ :")
+print("")
+operacion =input("Ingresa las 3 primer letras de la operacion quieres hacer, ejemplo: resta=res ᓚ₍ ^. .^₎:")
+print("")
+
+if operacion == "sum":
+    print("La suma sale",(int(resultado) + int(resultado2)),"ദ്ദി ˉ͈̀꒳ˉ͈́ )✧")
+elif operacion == "res" :
+        print("La resta sale",(int(resultado) - int(resultado2)),"ദ്ദി ˉ͈̀꒳ˉ͈́ )✧")
+else:
+     ""
+      
+
+if operacion == "mul" :
+    print("La multplicacion sale",(int(resultado) * int(resultado2)),"ദ്ദി ˉ͈̀꒳ˉ͈́ )✧")
+elif operacion == "div":
+     print("La division sale ",(int(resultado) / int(resultado2)),"ദ്ദി ˉ͈̀꒳ˉ͈́ )✧")
+else:
+     ("")
+
